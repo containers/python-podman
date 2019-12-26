480dd047475000e66295ddf5cfb90ea4a479f5ca
Jonathan G. Underwood <jonathan.underwood@gmail.com>
2019-12-26 16:47:26 +0000

    Make test/podman_testcase.py conform to black

--- test/podman_testcase.py
+++ test/podman_testcase.py
@@ -9,7 +9,7 @@ import unittest
 
 from varlink import VarlinkError
 
-MethodNotImplemented = 'org.varlink.service.MethodNotImplemented'
+MethodNotImplemented = "org.varlink.service.MethodNotImplemented"
 
 
 class PodmanTestCase(unittest.TestCase):
@@ -18,7 +18,7 @@ class PodmanTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Fixture to setup podman test case."""
-        if hasattr(PodmanTestCase, 'alpine_process'):
+        if hasattr(PodmanTestCase, "alpine_process"):
             PodmanTestCase.tearDownClass()
 
         def run_cmd(*args):
@@ -28,61 +28,60 @@ class PodmanTestCase(unittest.TestCase):
                     cmd,
                     close_fds=True,
                     stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE)
+                    stderr=subprocess.PIPE,
+                )
                 out, err = pid.communicate()
             except OSError as e:
-                print('{}: {}({})'.format(cmd, e.strerror, e.returncode))
+                print("{}: {}({})".format(cmd, e.strerror, e.returncode))
             except ValueError as e:
-                print('{}: {}'.format(cmd, e.message))
+                print("{}: {}".format(cmd, e.message))
                 raise
             else:
                 return out.strip()
 
-        tmpdir = os.environ.get('TMPDIR', '/tmp')
+        tmpdir = os.environ.get("TMPDIR", "/tmp")
         podman_args = [
-            '--storage-driver=vfs',
-            '--cgroup-manager=cgroupfs',
-            '--root={}/crio'.format(tmpdir),
-            '--runroot={}/crio-run'.format(tmpdir),
-            '--cni-config-dir={}/cni/net.d'.format(tmpdir),
+            "--storage-driver=vfs",
+            "--cgroup-manager=cgroupfs",
+            "--root={}/crio".format(tmpdir),
+            "--runroot={}/crio-run".format(tmpdir),
+            "--cni-config-dir={}/cni/net.d".format(tmpdir),
         ]
 
-        run_podman = functools.partial(run_cmd, ['podman'], podman_args)
+        run_podman = functools.partial(run_cmd, ["podman"], podman_args)
 
-        id = run_podman(['pull', 'alpine'])
-        setattr(PodmanTestCase, 'alpine_id', id)
+        id = run_podman(["pull", "alpine"])
+        setattr(PodmanTestCase, "alpine_id", id)
 
-        run_podman(['pull', 'busybox'])
-        run_podman(['images'])
+        run_podman(["pull", "busybox"])
+        run_podman(["images"])
 
-        run_cmd(['rm', '-f', '{}/alpine_gold.tar'.format(tmpdir)])
-        run_podman([
-            'save', '--output', '{}/alpine_gold.tar'.format(tmpdir), 'alpine'
-        ])
+        run_cmd(["rm", "-f", "{}/alpine_gold.tar".format(tmpdir)])
+        run_podman(
+            ["save", "--output", "{}/alpine_gold.tar".format(tmpdir), "alpine"]
+        )
 
         PodmanTestCase.alpine_log = open(
-            os.path.join('/tmp/', 'alpine.log'), 'w')
+            os.path.join("/tmp/", "alpine.log"), "w"
+        )
 
-        cmd = ['podman']
+        cmd = ["podman"]
         cmd.extend(podman_args)
         # cmd.extend(['run', '-d', 'alpine', 'sleep', '500'])
-        cmd.extend(['run', '-dt', 'alpine', '/bin/sh'])
+        cmd.extend(["run", "-dt", "alpine", "/bin/sh"])
         PodmanTestCase.alpine_process = subprocess.Popen(
-            cmd,
-            stdout=PodmanTestCase.alpine_log,
-            stderr=subprocess.STDOUT,
+            cmd, stdout=PodmanTestCase.alpine_log, stderr=subprocess.STDOUT,
         )
 
         PodmanTestCase.busybox_log = open(
-            os.path.join('/tmp/', 'busybox.log'), 'w')
+            os.path.join("/tmp/", "busybox.log"), "w"
+        )
 
-        cmd = ['podman']
+        cmd = ["podman"]
         cmd.extend(podman_args)
-        cmd.extend(['create', 'busybox'])
+        cmd.extend(["create", "busybox"])
         PodmanTestCase.busybox_process = subprocess.Popen(
-            cmd,
-            stdout=PodmanTestCase.busybox_log,
-            stderr=subprocess.STDOUT,
+            cmd, stdout=PodmanTestCase.busybox_log, stderr=subprocess.STDOUT,
         )
         # give podman time to start ctnr
         time.sleep(2)
@@ -97,12 +96,12 @@ class PodmanTestCase(unittest.TestCase):
         try:
             PodmanTestCase.alpine_process.kill()
             assert 0 == PodmanTestCase.alpine_process.wait(500)
-            delattr(PodmanTestCase, 'alpine_process')
+            delattr(PodmanTestCase, "alpine_process")
 
             PodmanTestCase.busybox_process.kill()
             assert 0 == PodmanTestCase.busybox_process.wait(500)
         except Exception as e:
-            print('Exception: {}'.format(e))
+            print("Exception: {}".format(e))
             raise
 
     @contextlib.contextmanager
